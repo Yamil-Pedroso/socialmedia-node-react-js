eb658e186db34919306e09ba87c5bb3aac5b17cb
Yami Pedroso-AI <yamirovinci@gmail.com>
2023-11-15 18:23:15 +0100

    super

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -1,12 +1,12 @@
-import { Typography, useTheme } from "@mui/material";
-import FlexBetween from "components/FlexBetween";
-import WidgetWrapper from "components/WidgetWrapper";
+import { Typography, useTheme } from '@mui/material'
+import FlexBetween from 'components/FlexBetween'
+import WidgetWrapper from 'components/WidgetWrapper'
 
 const AdvertWidget = () => {
-  const { palette } = useTheme();
-  const dark = palette.neutral.dark;
-  const main = palette.neutral.main;
-  const medium = palette.neutral.medium;
+  const { palette } = useTheme()
+  const dark = palette.neutral.dark
+  const main = palette.neutral.main
+  const medium = palette.neutral.medium
 
   return (
     <WidgetWrapper>
@@ -20,8 +20,8 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/info4.jpeg"
-        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+        src="https://linkto-mev1-0a208942f7c4.herokuapp.com/assets/info4.jpeg"
+        style={{ borderRadius: '0.75rem', margin: '0.75rem 0' }}
       />
       <FlexBetween>
         <Typography color={main}>MikaCosmetics</Typography>
@@ -32,7 +32,7 @@ const AdvertWidget = () => {
         is exfoliating skin and shining like light.
       </Typography>
     </WidgetWrapper>
-  );
-};
+  )
+}
 
-export default AdvertWidget;
+export default AdvertWidget
